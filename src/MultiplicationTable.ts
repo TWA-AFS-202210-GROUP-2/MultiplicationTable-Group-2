25f5e89401948075d423196ea3435c931dcaddb3
Jiazhen Yang <JYang56@slb.com>
2022-10-25 17:22:11 +0800

    feat: add test

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,44 +1,40 @@
 import { start } from "repl"
 
 export class MultiplicationTable {
-  public render(start: number, end: number): string{
-    return '1*1=1'
-  }
-}
-
-export function render(start:number,end:number):string{
-  if(isValid(start,end)){
-    return getResult(start,end)
-  }
-  else{
-    return ''
-  }
-}
-
-function isValid(start:number,end:number):boolean{
-  return isGreaterthan(start,end) && isInRange(start) && isInRange(end)
-}
-
-function isGreaterthan(start:number,end:number):boolean{
-  return start<=end
-}
-
-function isInRange(num:number):boolean{
-  return num<=10&&num>1
-}
-
-function getResult(start:number,end:number){
-  let result = ''
-  let i = start
-  let j = start
-  while(i<=end){
-    result = result+i.toString()+'*'+j.toString()+'='+(i*j).toString()+'  '
-    j=j+1
-    if(j>i){
-      result = result+'\n'
-      j = start
-      i = i+1
+  public render(start:number,end:number):string{
+    if(this.isValid(start,end)){
+      return this.getResult(start,end)
+    }
+    else{
+      return ''
     }
   }
-  return result
+
+  private isValid(start:number,end:number):boolean{
+    return this.isGreaterthan(start,end) && this.isInRange(start) && this.isInRange(end)
+  }
+
+  private isGreaterthan(start:number,end:number):boolean{
+    return start<=end
+  }
+
+  private isInRange(num:number):boolean{
+    return num<=10&&num>=1
+  }
+
+  private getResult(start:number,end:number){
+    let result = ''
+    let i = start
+    let j = start
+    while(i<=end){
+      result = result+i.toString()+'*'+j.toString()+'='+(i*j).toString()+'  '
+      j=j+1
+      if(j>i){
+        result = result+'\n'
+        j = start
+        i = i+1
+      }
+    }
+    return result
+  }
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -11,6 +11,19 @@ describe('Multiplication Table', () => {
     const rendered = table.render(start, end)
 
     //then
-    expect(rendered).toBe('1*1=1')
+    expect(rendered).toBe('1*1=1  \n')
+  })
+
+  it('should render multiplication table of (1, 2)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 1
+    const end = 2
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('1*1=1  \n2*1=2  2*2=4  \n')
   })
 })

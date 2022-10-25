e701d53f4c5dfb4ee93e349ee662b6e6e8db6d10
Jiahui Xiong <jxiong5@slb.com>
2022-10-25 18:33:16 +0800

    feat: complete multiplication table

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,59 @@
 export class MultiplicationTable {
   public render(start: number, end: number): string{
-    return '1*1=1'
+    let multiplicationTable=''
+    const isCorrect=this.isCorrectInput(start,end)
+    if(isCorrect){
+      const expressions=this.buildExpressions(start,end)
+      multiplicationTable=this.generateMultiplicationTable(expressions)
+    }
+    return multiplicationTable
+  }
+
+  isValidInput(start:number,end:number):boolean{
+    const isInt:boolean=Math.floor(start)===start && Math.floor(end)===end
+    const isOneToTen:boolean=start<=10 && start>=1 && end<=10 && end>=1
+    return isInt && isOneToTen
+  }
+
+  isStartLessAndEqualEnd(start:number,end:number):boolean{
+    return start<=end
+  }
+
+  isCorrectInput(start:number,end:number):boolean{
+    return this.isValidInput(start,end) && this.isStartLessAndEqualEnd(start,end)
+  }
+
+  buildExpressions(start:number,end:number):string[][]{
+    const expressions:string[][]=[]
+    const rangeArr=this.generateArray(start,end)
+    rangeArr.forEach((firstItem:number,index:number)=>{
+      expressions.push([])
+      rangeArr.slice(0,index+1).forEach((secondItem:number)=>{
+        const result=firstItem*secondItem
+        const expression=`${secondItem}*${firstItem}=${result}`
+        expressions[expressions.length-1].push(expression)
+      })
+    })
+    return expressions
+  }
+
+  generateArray(start:number, end:number):number[] {
+    return Array.from(new Array(end + 1).keys()).slice(start)
+  }
+
+  generateMultiplicationTable(expressions:string[][]):string{
+    let result=''
+    expressions.forEach((firstItem:string[],firstIndex:number)=>{
+      firstItem.forEach((secondItem:string,index:number)=>{
+        result+=`${secondItem}`
+        if(index!==firstItem.length-1){
+          result+='  '
+        }
+      })
+      if(firstIndex!==expressions.length-1){
+        result+='\n'
+      }
+    })
+    return result
   }
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -13,4 +13,43 @@ describe('Multiplication Table', () => {
     //then
     expect(rendered).toBe('1*1=1')
   })
+
+  it('should render "" if start larger than end',()=>{
+    const table=new MultiplicationTable()
+    const start=3
+    const end=1
+    const rendered=table.render(start,end)
+    expect(rendered).toBe('')
+  })
+
+  it('should render "" if the start number and the end number is not integer in a range of 1 to 10',()=>{
+    const table=new MultiplicationTable()
+    const start=1
+    const end=11
+    const rendered=table.render(start,end)
+    expect(rendered).toBe('')
+  })
+
+  it('should render multiplication table of (1, 3)',()=>{
+    const table=new MultiplicationTable()
+    const start=1
+    const end=3
+    const rendered=table.render(start,end)
+    const expected=`1*1=1
+1*2=2  2*2=4
+1*3=3  2*3=6  3*3=9`
+    expect(rendered).toBe(expected)
+  })
+
+  it('should render multiplication table of (2, 5)',()=>{
+    const table=new MultiplicationTable()
+    const start=2
+    const end=5
+    const rendered=table.render(start,end)
+    const expected=`2*2=4
+2*3=6  3*3=9
+2*4=8  3*4=12  4*4=16
+2*5=10  3*5=15  4*5=20  5*5=25`
+    expect(rendered).toBe(expected)
+  })
 })

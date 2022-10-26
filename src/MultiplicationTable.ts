53d956abe814f7e5e3ea3fc4fb495cb387b613e2
hsun15 <hsun15@slb.com>
2022-10-26 09:14:30 +0800

    no task

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,42 @@
 export class MultiplicationTable {
   public render(start: number, end: number): string{
-    return '1*1=1'
+    if (!this.startIsNotGreaterThanEnd(start,end))
+      return ""
+    
+    if ((!this.numInBetween1and10(start)) ||(!this.numInBetween1and10(end)))
+      return "num is out of range"
+    return this.generateMultiplication(start,end)
   }
+
+  startIsNotGreaterThanEnd(startNum:number,endNum:number):boolean
+  {
+    return startNum<endNum||startNum==endNum 
+  }
+
+  numInBetween1and10(num:number):boolean
+  {
+    return num<11&&num>-1
+  }
+
+  generateMultiplication(startNum:number,endNum:number):string
+  {
+  var muiltiplicationStr=""
+  for(let i=startNum;i<=endNum;i++)
+    for(let j=startNum;j<=endNum;j++)
+    {
+      muiltiplicationStr=muiltiplicationStr+this.cellString(j,i)
+      if(j<i) muiltiplicationStr=muiltiplicationStr+"  "
+    }
+    muiltiplicationStr=muiltiplicationStr+"\n"
+  return muiltiplicationStr
+  }
+
+  cellString(num1:number,num2:number):string
+  {
+  return num1+"*"+num2+"="+num1*num2
+  }
+
+
+
 }
+
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,16 +1,49 @@
 import {MultiplicationTable} from '../src/MultiplicationTable'
 
 describe('Multiplication Table', () => {
-  it('should render multiplication table of (1, 1)', () => {
-    // given
-    const table = new MultiplicationTable()
-    const start = 1
-    const end = 1
+  // it('should render multiplication table of (1, 1)', () => {
+  //   // given
+  //   const table = new MultiplicationTable()
+  //   const start = 1
+  //   const end = 1
 
-    //when
-    const rendered = table.render(start, end)
+  //   //when
+  //   const rendered = table.render(start, end)
 
-    //then
-    expect(rendered).toBe('1*1=1')
-  })
+  //   //then
+  //   expect(rendered).toBe('1*1=1')
+  // })
+
+  // it('a simple example',()=>{
+  //   const table = new MultiplicationTable()
+  //   const start = 2
+  //   const end = 4
+
+  //   //when
+  //   const rendered = table.render(start, end)
+  //   expect(rendered).toBe('2*2=4\n2*3=6  3*3=9\n2*4=8  3*4=12  4*4=16\n')
+  // }
+  // )
+
+  // it('start number > end number',()=>{
+  //   const table = new MultiplicationTable()
+  //   const start = 4
+  //   const end = 2
+
+  //   //when
+  //   const rendered = table.render(start, end)
+  //   expect(rendered).toBe("")
+  // }
+  // )
+
+  // it('start number not in range',()=>{
+  //   const table = new MultiplicationTable()
+  //   const start = 4
+  //   const end = 2
+
+  //   //when
+  //   const rendered = table.render(start, end)
+  //   expect(rendered).toBe('num is out of range')
+  // }
+  // )
 })
